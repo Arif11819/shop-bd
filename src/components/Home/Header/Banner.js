bbc962ee95e3ccc604287d9f93f9fbbd3a4abf47
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-09-28 14:33:11 +0600

    added file after cross icon implement

--- src/components/Home/Header/Banner.js
+++ src/components/Home/Header/Banner.js
@@ -5,7 +5,10 @@ import { BsCircleFill } from "react-icons/bs";
 const Banner = () => {
     return (
         <div className='mt-4 mb-20'>
-            <h4>Invite Friends and get 50% off on your next purchase <a className='text-blue-600 pl-2' href="#">Invite Now</a> </h4>
+            <div className='flex items-center justify-center'>
+                <h4 className='ml-[500px]'>Invite Friends and get 50% off on your next purchase <a className='text-blue-600 pl-2' href="#">Invite Now</a> </h4>
+                <p className='ml-[500px]'><img className='w-[13px] h-[13px]' src="https://i.ibb.co/xjkd27n/Vector-7.png" alt="" /></p>
+            </div>
             <div className='flex mt-4'>
                 <div>
                     <img className='w-[1028px] h-[596px]' src="https://i.ibb.co/DDRk9JC/Rectangle-374-1.png" alt="" />
@@ -20,11 +23,6 @@ const Banner = () => {
                 <BsCircleFill className='third-circle w-[8px] h-[8px]'></BsCircleFill>
                 <BsCircleFill className='fourth-circle w-[8px] h-[8px]'></BsCircleFill>
                 <BsCircleFill className='fifth-circle w-[8px] h-[8px]'></BsCircleFill>
-                {/* <img className='w-[14px] h-[14px] first-circle' src="https://i.ibb.co/W5b6YzQ/Ellipse-45-2.png" alt="" />
-                <img className='w-[14px] h-[14px]' src="https://i.ibb.co/W5b6YzQ/Ellipse-45-2.png" alt="" />
-                <img className='w-[14px] h-[14px]' src="https://i.ibb.co/W5b6YzQ/Ellipse-45-2.png" alt="" />
-                <img className='w-[14px] h-[14px]' src="https://i.ibb.co/W5b6YzQ/Ellipse-45-2.png" alt="" />
-                <img className='w-[14px] h-[14px]' src="https://i.ibb.co/W5b6YzQ/Ellipse-45-2.png" alt="" /> */}
             </div>
         </div>
     );
